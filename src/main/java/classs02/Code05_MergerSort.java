2659f4f70c5b8b01d89168157d8416a463c4fea4
Your Name <you@example.com>
2024-08-18 23:21:40 +0800

    归并排序

---
+++ src/main/java/classs02/Code05_MergerSort.java
@@ -0,0 +1,14 @@
+package classs02;
+
+/**
+ * 归并排序
+ * 思想：将一个数组分为两部分，分别对两部分进行排序，然后分别进行比较
+ * @author: dongxiaoxu
+ * @date: 2024/8/18
+ */
+public class Code05_MergerSort {
+    //todo
+    public static void MergeSort(int[] arr){
+
+    }
+}
---
+++ src/main/java/classs02/Code06_SmallSum.java
@@ -0,0 +1,10 @@
+package classs02;
+
+/**
+ * 小和
+ * @author:dongxiaoxu
+ * @date: 2024/8/18
+ */
+public class Code06_SmallSum {
+    //todo
+}
